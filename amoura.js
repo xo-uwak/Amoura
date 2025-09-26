07dc7e96fd60f662434b69c18c4525c91eab711e
Uwakmfon James <contactuwak@gmail.com>
2025-09-26 01:47:28 +0100

    AMOURA Homepage version 1

---
+++ amoura.js
@@ -0,0 +1,9 @@
+/*code for navigation link toggle open and close*/
+let navLinks = document.getElementById('navLinks');
+
+function showMenu(){
+  navLinks.style.right = '0';
+}
+function hideMenu(){
+  navLinks.style.right ='-200px';
+}
