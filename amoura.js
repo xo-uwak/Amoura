98904882899d83a56ef49f7975fbe5952e6753c4
Uwakmfon James <contactuwak@gmail.com>
2025-10-30 20:40:57 +0100

    amoura complete

--- amoura.js
+++ amoura.js
@@ -7,3 +7,15 @@ function showMenu(){
 function hideMenu(){
   navLinks.style.right ='-200px';
 }
+
+/* to send order to mail*/
+document.querySelector('.order-form form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const product = document.getElementById('product').value;
+  const message = document.getElementById('message').value;
+  const mailtoLink = `mailto:uwakreadsgmail.com?subject=New Order from ${name}&body=Name: ${name}%0AEmail: ${email}%0AProduct: ${product}%0AOrder Details: ${message}`;
+  window.location.href = mailtoLink;
+  alert('Your order request has been processed.');
+});
