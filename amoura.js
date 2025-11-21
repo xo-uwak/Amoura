0188a87389f8a8f49f500b75badca625121b9e84
Uwakmfon James <contactuwak@gmail.com>
2025-11-21 18:54:48 +0100

    changed Amoura.html to index.html

--- amoura.js
+++ amoura.js
@@ -114,23 +114,3 @@ function editReview(id) {
 
 
 
-/* to send order to mail
-document.querySelector('.order-form form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const product = document.getElementById('product').value;
-  const message = document.getElementById('message').value;
-  const mailtoLink = `mailto:uwakreadsgmail.com?subject=New Order from ${name}&body=Name: ${name}%0AEmail: ${email}%0AProduct: ${product}%0AOrder Details: ${message}`;
-  window.location.href = mailtoLink;
-  alert('Your order request has been processed.');
-});*/
-
-/*
-let review;
-document.getElementById("review-submit-btn").onclick = function(){
-  review = document.getElementById("notess").value;
-  document.getElementById("review-input").textContent = review;
-  console.log(review)
-};
-*/
